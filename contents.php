755aef90310b9e317eb551f89d771905310cd8b9
lyn-tani <lyntani@uw.edu>
2022-03-14 03:45:22 -0700

    Css updates to navbar.

--- contents.php
+++ contents.php
@@ -13,41 +13,40 @@
  
 <body> 
     <!-- START -- Add HTML code for the top menu section (navigation bar) --> 
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Music Chart</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+	 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+         <a class="navbar-brand"><img id="charted-logo" src="charted-1.png"width="100px" height="100px" ></a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
 
-			<div class="collapse navbar-collapse" id="navbarColor02">
-				<ul class="navbar-nav me-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="index.php">Home</a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="contents.php">Contents
-							<span class="visually-hidden">(current)</span>
-						</a>
-					</li>
-					<!-- <li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<a class="dropdown-item" href="#">Something else here</a>
-							<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Separated link</a>
-						</div>
-					</li> -->
-				</ul>
-				<form class="d-flex">
-					<input class="form-control me-sm-2" type="text" placeholder="Search">
-					<button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-				</form>
-			</div>
-		</div>
-	</nav>
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home
+              <span class="visually-hidden"></span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="contents.php">Contents</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-outline-light" href="#">Login</a>
+          </li>
+        <!--  <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li> -->
+        </ul>
+        </div>
+      </div>
+    </nav>
     <!-- END -- Add HTML code for the top menu section (navigation bar) --> 
 	<div class="jumbotron"> 
         <p class="lead">Current Music, Albums, and Artists we have catalogued<p> 
--- index.php
+++ index.php
@@ -7,42 +7,45 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
         <title>Music Chart</title> 
         <!-- add a reference to the external stylesheet --> 
-        <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css"> 
+        <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
     </head> 
  
-<body> 
-    <!-- START -- Add HTML code for the top menu section (navigation bar) --> 
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">Music Chart</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+<body>
+	<!-- START -- Add HTML code for the top menu section (navigation bar) -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand"><img id="charted-logo" src="charted-1.png"width="100px" height="100px" ></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-			<div class="collapse navbar-collapse" id="navbarColor02">
-				<ul class="navbar-nav me-auto">
-					<li class="nav-item">
-						<a class="nav-link active" href="index.php">Home
-							<span class="visually-hidden">(current)</span>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="contents.php">Contents</a>
-					</li>
-					<!-- <li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<a class="dropdown-item" href="#">Something else here</a>
-							<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Separated link</a>
-						</div>
-					</li> -->
-				</ul>	
-			</div>
-		</div>
-	</nav>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="index.php">Home
+                <span class="visually-hidden"></span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contents.php">Contents</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-outline-light" href="#">Login</a>
+            </li>
+          <!--  <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Separated link</a>
+              </div>
+            </li> -->
+          </ul>
+    			</div>
+    		</div>
+    	</nav>
     <!-- END -- Add HTML code for the top menu section (navigation bar) --> 
 	<div class="jumbotron"> 
         <h1 class="display-3">Welcome to Music Chart!</h1> 
