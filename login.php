8b008b192969e91aecd05881d54b3ebfb01847a6
lyn-tani <lyntani@uw.edu>
2022-03-14 22:39:00 -0700

    login update

--- login.php
+++ login.php
@@ -6,7 +6,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Assignment 4 Demo</title>
         <!-- add a reference to the external stylesheet -->
         <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
     </head>
